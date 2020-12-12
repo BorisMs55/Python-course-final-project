522516eb9f2cb413aa17ac44f65a47e149ef9893
BorisMs55 <62842465+BorisMs55@users.noreply.github.com>
2020-12-12 18:58:42 +0200

    git commit -a -m "Added 2 new tables"
     Please enter the commit message for your changes. Lines starting

--- hr/employee/models.py
+++ hr/employee/models.py
@@ -1,3 +1,34 @@
+from djmoney.models.fields import MoneyField
 from django.db import models
 
+
 # Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=75)
+    last_name = models.CharField(max_length=75)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=16)
+    hire_date = models.DateField()
+    salary = MoneyField(max_digits=14, decimal_places=2, default_currency='ILS')
+    commission_pct = models.DecimalField(max_digits=4, decimal_places=2)
+    manager = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
+    job = models.ForeignKey('Job', null=True, on_delete=models.SET_NULL)
+    department = models.ForeignKey('Department', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Job(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
